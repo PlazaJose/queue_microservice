6ad8c2ad526730f7211d19ed2d77d805d2fda59a
PlazaJose <plaza.jose@correounivalle.edu.co>
2025-06-12 23:43:27 -0500

    prueba_vercel_6

--- events.js
+++ events.js
@@ -16,7 +16,7 @@ async function sendEvent(eventType, eventData) {
 
 async function start_match(q_data) {
     try {
-        const response = await fetch('https://api-gateway-nine-orcin.vercel.app/game/match/create_match', {
+        const response = await fetch('https://match-microservice.vercel.app/match/create_match', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(q_data)
