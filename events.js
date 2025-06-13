d26b50f35798b94a6aad2b3ae486b332e11dd6df
PlazaJose <plaza.jose@correounivalle.edu.co>
2025-06-13 00:16:13 -0500

    prueba_vercel_9

--- events.js
+++ events.js
@@ -15,7 +15,7 @@ async function sendEvent(eventType, eventData) {
 }
 
 async function start_match(q_data) {
-    console.log("starting match...")
+    console.log("starting match...");
     try {
         const response = await fetch('https://match-microservice.vercel.app/match/create_match', {
             method: 'POST',
